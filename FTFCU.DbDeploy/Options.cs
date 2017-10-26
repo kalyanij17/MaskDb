309e363f092f7c7c6b517598b81fc9f66a2e27e4
KalyaniJ <KalyaniJ@LT-IT209>
2017-10-26 14:43:00 -0700

    Octopus deployment

---
+++ FTFCU.DbDeploy/Options.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTFCU.DbDeploy
+{
+    class Options
+    {
+        [Option('c', "connectionstring", Required = false,
+    HelpText = "This is the connectiong string to the sql database .")]
+        public string ConnectionString { get; set; }
+        [Option('t', "targetdatabase", Required = true,
+   HelpText = "target database required.")]
+        public string TargetDatabase { get; set; }
+
+        [Option('d', " dac directory", Required = true,
+   HelpText = "dac directory.")]
+        public string DacDictionary { get; set; }
+
+        [Option('p', " dac profile", Required = true,
+   HelpText = "dac profile required.")]
+        public string DacProfile { get; set; }
+
+        [Option('h', " help", Required = false,
+    HelpText = "These are parameters used to build database.  \n" +
+            "c connectionstring -  connectionstring to sql database. \n" +
+            "t target database - database name that needs to be built \n "+
+            "d dac dictionary - dacpac name \n" +
+            "p dac profile - profile xml with sql scritps "
+            )]
+        public string Help { get; set; }
+
+    }
+}
--- FTFCU.DbDeploy/Program.cs
+++ FTFCU.DbDeploy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.SqlServer.Dac;
+using System;
 using System.Configuration;
 using System.IO;
 
@@ -8,18 +9,75 @@ namespace FTFCU.DbDeploy
     {
         static void Main(string[] args)
         {
+            var Options = new Options();
+            string vDacDictionary = null;
+            string vDacProfile = null;
+            string vtargetDatabase = null;
+            string connectionString = null;
+            bool isValid = false;
+            
+                try
+                {
+                    isValid = CommandLine.Parser.Default.ParseArguments(args, Options);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("there is an error in command line arguments , try reading it from configuration " + e.Message);
+                   
+                }
 
-            string path = Directory.GetCurrentDirectory() ;
-            var dacPackDirectory = path+ $@"\FTFCU.MaskDatabase.dacpac";
-            var dacPackProfile = path + $@"\FTFCU.MaskDatabase.publish.xml";
-            var targetDatabase =  "Mask";
-            var connectionString = ConfigurationManager.ConnectionStrings["Mask"].ConnectionString;
+           
 
+            if (isValid)
+            {
+                vDacDictionary = Options.DacDictionary;
+                vDacProfile = Options.DacProfile;
+                vtargetDatabase = Options.TargetDatabase;
+                connectionString = ConnectionStringResolver(Options, connectionString);
+            }
+            else
+            {
 
-            var dacService = new DacServices(connectionString);
-            var dp = DacPackage.Load(dacPackDirectory);
-            var dacProfile = DacProfile.Load(dacPackProfile);
-            dacService.Deploy(dp, targetDatabase, true, dacProfile.DeployOptions);
+                vDacDictionary = ConfigurationManager.AppSettings.Get("dac directory");
+                vDacProfile = ConfigurationManager.AppSettings.Get("dac profile");
+                vtargetDatabase = ConfigurationManager.AppSettings.Get("targetdatabase");
+                connectionString = ConfigurationManager.ConnectionStrings["Mask"].ConnectionString;
+            }
+            string path = Directory.GetCurrentDirectory();
+           // connectionString = ConnectionStringResolver(Options, connectionString);
+
+            try
+            {
+                var dp = DacPackage.Load(path + $@"\" + vDacDictionary);
+                var dacService = new DacServices(connectionString);
+                var dacProfile = DacProfile.Load(path + $@"\" + vDacProfile);
+                dacService.Message += (object sender, Microsoft.SqlServer.Dac.DacMessageEventArgs eventArgs) => Console.WriteLine(eventArgs.Message.Message);
+
+                dacService.Deploy(dp, vtargetDatabase, true, dacProfile.DeployOptions);
+
+                Console.WriteLine("Deployment completed successfully !!!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in the deployment. please correct the input " + ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+
+        
+
+        private static string ConnectionStringResolver(Options Options, string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(Options.ConnectionString))
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["Mask"].ConnectionString;
+            }else
+            {
+                connectionString = Options.ConnectionString;
+            }
+
+            return connectionString;
         }
     }
 }
